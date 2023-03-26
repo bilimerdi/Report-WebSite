3ddfb0492d9989f551c0c98cdc396e8fb9f1febb
bilimerdi <orunerdi186@gmail.com>
2023-03-27 01:09:09 +0300

    Added Footer.js
    Footer.js file added to component.İnstall react-router-dom and react-icons library.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Footer from "./components/Footer";
+
 const App = () => {
-  return <div>hello,it is a Report Web</div>;
+  return (
+    <div>
+      hello,it is a Report Web
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
