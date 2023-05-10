4a54b087bf0147466382eb10acf4f5f495014275
bilimerdi <orunerdi186@gmail.com>
2023-05-10 19:36:50 +0300

    Added avatar,stack component from material uı

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Footer from "./components/Footer";
 import UserForm from "./components/UserForm";
 
 const App = () => {
   return (
     <div>
       <UserForm></UserForm>
-      <Footer />
     </div>
   );
 };
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,37 +1,58 @@
 import React from "react";
 import "./UserForm.css";
-import { Button, TextField, Select, MenuItem, InputLabel } from "@mui/material";
+import man1 from "../assets/man1.png";
+import man2 from "../assets/man2.jpg";
+import man3 from "../assets/man3.webp";
+import man4 from "../assets/man4.ico";
+import woman1 from "../assets/woman1.jpg";
+import woman2 from "../assets/woman2.png";
+import {
+  Button,
+  TextField,
+  Select,
+  MenuItem,
+  InputLabel,
+  Avatar,
+  Stack,
+} from "@mui/material";
 
 const UserForm = () => {
   return (
-    <div>
-      <div className="form-border">
-        <TextField
-          className="input"
-          label="Full Name"
-          variant="outlined"
-        ></TextField>
-        <TextField
-          className="input"
-          label="Username"
-          variant="outlined"
-        ></TextField>
-        <TextField
-          className="input"
-          label="Email Adress"
-          variant="outlined"
-        ></TextField>
-        <InputLabel>Role</InputLabel>
-        <Select label="Role" placeholder="Role">
-          <MenuItem>Contributor</MenuItem>
-          <MenuItem>Subscriber</MenuItem>
-          <MenuItem>Author</MenuItem>
-          <MenuItem>Administrator</MenuItem>
-        </Select>
-        <Button className="button" variant="contained">
-          Create User
-        </Button>
-      </div>
+    <div className="form-border">
+      <TextField
+        className="input"
+        label="Full Name"
+        variant="outlined"
+      ></TextField>
+      <TextField
+        className="input"
+        label="Username"
+        variant="outlined"
+      ></TextField>
+      <TextField
+        className="input"
+        label="Email Adress"
+        variant="outlined"
+      ></TextField>
+      <InputLabel>Role</InputLabel>
+      <Select label="Role" placeholder="Role">
+        <MenuItem>Contributor</MenuItem>
+        <MenuItem>Subscriber</MenuItem>
+        <MenuItem>Author</MenuItem>
+        <MenuItem>Administrator</MenuItem>
+      </Select>
+      <div>Select Avatar</div>
+      <Stack direction="row" spacing={2}>
+        <Avatar variants="rounded" src={man1} />
+        <Avatar variant="rounded" src={man2} />
+        <Avatar variant="rounded" src={man3} />
+        <Avatar variant="rounded" src={man4} />
+        <Avatar variant="rounded" src={woman1} />
+        <Avatar variant="rounded" src={woman2} />
+      </Stack>
+      <Button className="button" variant="contained">
+        Create User
+      </Button>
     </div>
   );
 };
