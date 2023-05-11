8dc621f7833f98913eb4e2ac26b873307fce9239
bilimerdi <orunerdi186@gmail.com>
2023-05-11 20:57:11 +0300

    Update AvatarCreate.js file
    Deleted assets file because pictures taken from the internet

--- src/components/AvatarCreate.js
+++ src/components/AvatarCreate.js
@@ -1,10 +1,4 @@
 import React from "react";
-import man1 from "../assets/man1.png";
-import man2 from "../assets/man2.jpg";
-import man3 from "../assets/man3.webp";
-import man4 from "../assets/man4.ico";
-import woman1 from "../assets/woman1.jpg";
-import woman2 from "../assets/woman2.png";
 import { Avatar, Stack } from "@mui/material";
 
 const AvatarCreate = () => {
@@ -12,12 +6,30 @@ const AvatarCreate = () => {
     <div>
       <div style={{ marginBottom: 10 }}>Select Avatar</div>
       <Stack direction="row" spacing={2} sx={{ marginBottom: 2 }}>
-        <Avatar variant="rounded" src={man1} />
-        <Avatar variant="rounded" src={man2} />
-        <Avatar variant="rounded" src={man3} />
-        <Avatar variant="rounded" src={man4} />
-        <Avatar variant="rounded" src={woman1} />
-        <Avatar variant="rounded" src={woman2} />
+        <Avatar
+          variant="rounded"
+          src="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/2e/2e3dc3ce3332709e8ceda9da083f5a0afe9797d5.jpg"
+        />
+        <Avatar
+          variant="rounded"
+          src="https://avatars.steamstatic.com/418b05b6c8ac32c4e91ff26e9657db2e1db540b6_full.jpg"
+        />
+        <Avatar
+          variant="rounded"
+          src="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/db/db1e813230a14e06c3cd05fb4784942a7c2ec732.jpg"
+        />
+        <Avatar
+          variant="rounded"
+          src="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/b3/b3a4725d5456b7f4df5c9a93b6465e56b66f3e73.jpg"
+        />
+        <Avatar
+          variant="rounded"
+          src="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/05/05607d1073475924122712fce813e745b112b2db.jpg"
+        />
+        <Avatar
+          variant="rounded"
+          src="https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/b1/b154c144c014b2b8de8d130c71bccf3d0f23e707.jpg"
+        />
       </Stack>
     </div>
   );
